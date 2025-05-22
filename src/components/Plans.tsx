489317028f9c636f9b43385bca4aa0d734dc8bcd
leandrontenorio <leandro.ntenorio@gmail.com>
2025-05-22 00:16:23 -0300

    Update payment redirect URLs

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -153,7 +153,7 @@ function Plans() {
             email: user.email
           },
           back_urls: {
-            success: 'https://wondrous-yeot-7f10c4.netlify.app/payment/success',
+            success: 'https://wondrous-yeot-7f10c4.netlify.app/plan',
             failure: 'https://wondrous-yeot-7f10c4.netlify.app/payment/failure',
             pending: 'https://wondrous-yeot-7f10c4.netlify.app/payment/pending'
           },
